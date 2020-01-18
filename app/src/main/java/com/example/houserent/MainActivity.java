fe05a5e6c6f80bf51a8845b04cedb4c972b1424f
vivehjtv <vivekhjtv@gmail.com>
2020-01-18 23:41:05 +0530

    google login s

--- app/src/main/java/com/example/houserent/MainActivity.java
+++ app/src/main/java/com/example/houserent/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.houserent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_rent,btn_sell;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_rent=findViewById(R.id.rent);
+        btn_sell=findViewById(R.id.sell);
+        
+        btn_rent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),houselist.class));
+            }
+        });
     }
 }
