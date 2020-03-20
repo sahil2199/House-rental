8a83ece5e2aa80b9f7e424710caf3e059b5e428c
vivehjtv <vivekhjtv@gmail.com>
2020-03-20 22:57:07 +0530

    about

--- app/src/main/java/com/example/houserent/AboutUs.java
+++ app/src/main/java/com/example/houserent/AboutUs.java
@@ -3,12 +3,17 @@ package com.example.houserent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutUs extends AppCompatActivity {
+    TextView title,title1,des;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        title=findViewById(R.id.house_title);
+        title1=findViewById(R.id.house_title1);
+        des=findViewById(R.id.house_des);
     }
 }
