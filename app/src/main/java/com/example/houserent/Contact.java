c51b91d16a8ecbb74a4e8f2238b8ef647e956db6
vivekjethva <info.vivekhjtv18@gmail.com>
2020-06-14 22:28:57 +0530

    contact email

--- app/src/main/java/com/example/houserent/Contact.java
+++ app/src/main/java/com/example/houserent/Contact.java
@@ -4,10 +4,15 @@ package com.example.houserent;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.DataSnapshot;
@@ -20,16 +25,18 @@ public class Contact extends AppCompatActivity {
     private TextView contect,to,subject,message;
     private EditText memail,msubject,msg,mobile_no;
     private Button send_email;
+    ImageButton btCall;
     String sellerID;
     DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        send_email=findViewById(R.id.contact_button);
         memail = findViewById(R.id.pemail);
         msubject = findViewById(R.id.msubject);
         msg = findViewById(R.id.message_text);
-        mobile_no = findViewById(R.id.pnum1);
+
         sellerID = getIntent().getStringExtra("sellerId");
         databaseReference = FirebaseDatabase.getInstance().getReference();
         databaseReference.child("Users").child(sellerID).addValueEventListener(new ValueEventListener() {
@@ -46,6 +53,45 @@ public class Contact extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
+
         });
+        send_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail();
+            }
+        });
+
+        mobile_no = findViewById(R.id.pnum1);
+        btCall = findViewById(R.id.bt_call);
+        btCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phone = mobile_no.getText().toString();
+                if(phone.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please Enter Number",Toast.LENGTH_SHORT).show();
+                }else{
+                    String s = "tel:" + phone;
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    intent.setData(Uri.parse(s));
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+    private void sendMail(){
+        String recipientList = memail.getText().toString();
+        String[] recipients =  recipientList.split(",");
+        String subject = msubject.getText().toString();
+        String message = msg.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL,recipients);
+        intent.putExtra(Intent.EXTRA_SUBJECT,subject);
+        intent.putExtra(Intent.EXTRA_TEXT,message);
+
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent,"Choose an Email Client"));
+
     }
 }
