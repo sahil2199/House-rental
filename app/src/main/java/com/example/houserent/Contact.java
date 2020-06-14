8e38d6e8dbd13babd8943932e99dc4266809d0b9
vivekjethva <info.vivekhjtv18@gmail.com>
2020-06-14 12:33:32 +0530

    contact change

--- app/src/main/java/com/example/houserent/Contact.java
+++ app/src/main/java/com/example/houserent/Contact.java
@@ -4,9 +4,16 @@ package com.example.houserent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Contact extends AppCompatActivity {
-
+    private TextView contect,to,subject,message;
+    private EditText memail,msubject,msg,mobile_no;
+    private Button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
